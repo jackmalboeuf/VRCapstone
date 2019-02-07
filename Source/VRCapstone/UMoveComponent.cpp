62e155b31f30e80c12171e52bc8d66722c0c5eb4
Thomas Rohn <trohn54@gmail.com>
2019-02-07 17:56:44 -0600

    Movement Component Run Smoothly

--- Source/VRCapstone/UMoveComponent.cpp
+++ Source/VRCapstone/UMoveComponent.cpp
@@ -1,9 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UMoveComponent.h"
+#include "Runtime/Engine/Classes/Engine/Engine.h"
 #include "GameFramework/Actor.h"
-#include "Math/UnrealMathUtility.h"
-
+#include "Runtime/Core/Public/Math/UnrealMathUtility.h"
+#include <iostream>
 
 // Sets default values for this component's properties
 UUMoveComponent::UUMoveComponent()
@@ -11,7 +12,7 @@ UUMoveComponent::UUMoveComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
+	RegisterComponent();
 	// ...
 }
 
@@ -22,19 +23,28 @@ void UUMoveComponent::BeginPlay()
 	Super::BeginPlay();
 	spawnLocation = GetOwner()->GetActorLocation();
 	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Component Running"))
 }
 
 void UUMoveComponent::SetLocation()
 {
 	float x = FMath::RandRange((spawnLocation.X - wanderDistance), (spawnLocation.X + wanderDistance));
 	float y = FMath::RandRange((spawnLocation.Y - wanderDistance), (spawnLocation.Y + wanderDistance));
-	goalLocation = FVector(x,y, 0);
+	float z = GetOwner()->GetActorLocation().Z;
+	 goalLocation = FVector(x , y, z);
+
+	 GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, FString::Printf(TEXT("X: %f, Y: %f"), x, y));
 
 }
 
 void UUMoveComponent::Move()
 {
-	FVector V = goalLocation - GetOwner()->GetActorLocation();
+	
+	//TODO make uptime and downtime functionality
+	//FVector Direction = goalLocation - GetOwner()->GetActorLocation();
+	//Direction.Normalize();
+	//FVector NewLocation += Direction * GetOwner()->GetActorLocation();
+	GetOwner()->SetActorLocation(FMath::Lerp(GetOwner()->GetActorLocation(), goalLocation, 0.05f), true);
 }
 
 
@@ -51,16 +61,32 @@ void UUMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActor
 		//Change state to Moving
 		m_state = Moving;
 		timer = moveWaitTime;
+		SetLocation();
 	}
+
+	if (FMath::IsNearlyEqual(goalLocation.X, GetOwner()->GetActorLocation().X, 0.1f) &&
+		FMath::IsNearlyEqual(goalLocation.Y, GetOwner()->GetActorLocation().Y, 0.1f))
+	{
+
+		m_state = Standing;
+
+	}
+		
 	switch (m_state)
 	{
 	case UUMoveComponent::Moving:
+		UE_LOG(LogTemp, Warning, TEXT("Location is %s"), *GetOwner()->GetActorLocation().ToString());
+
 		Move();
-		m_state = Standing;
+
+		
 	case UUMoveComponent::Standing:
+		//SetLocation();
+		UE_LOG(LogTemp, Warning, TEXT("AI Standing"))
 		break;
 	default:
 		break;
 	}
+	//UE_LOG(LogTemp, Warning, TEXT("Component Running"))
 }
 
